6b854d51b002dd793bcb155cea2124da63fb993f
Johannes <DJBnjack@users.noreply.github.com>
2016-04-03 22:10:49 +0200

    Update amd.sh

--- amd.sh
+++ amd.sh
@@ -1,3 +1,4 @@
+tar xvf ./AMD-APP-SDK-linux-v2.9-1.599.381-GA-x64.tar.bz2
 ./AMD-APP-SDK-v2.9-1.599.381-GA-linux64.sh
 ln -s /opt/AMDAPPSDK-2.9-1 /opt/AMDAPP
 ln -s /opt/AMDAPP/include/CL /usr/include
