6da73ab096c3caecf3b6509803f76b085bff3c5c
Johannes <DJBnjack@users.noreply.github.com>
2016-04-03 22:13:33 +0200

    Update and rename amd.sh to amd1.sh

--- amd.sh
+++
@@ -1,6 +0,0 @@
-tar xvf ./AMD-APP-SDK-linux-v2.9-1.599.381-GA-x64.tar.bz2
-./AMD-APP-SDK-v2.9-1.599.381-GA-linux64.sh
-ln -s /opt/AMDAPPSDK-2.9-1 /opt/AMDAPP
-ln -s /opt/AMDAPP/include/CL /usr/include
-ln -s /opt/AMDAPP/lib/x86_64/* /usr/lib/
-ldconfig
---
+++ amd1.sh
@@ -0,0 +1,3 @@
+tar xvf ./AMD-APP-SDK-linux-v2.9-1.599.381-GA-x64.tar.bz2
+./AMD-APP-SDK-v2.9-1.599.381-GA-linux64.sh
+
