ecee103f26019c234bc5738a605e33f4183ffdfb
Johannes <DJBnjack@users.noreply.github.com>
2016-04-03 21:57:24 +0200

    Update importchain.sh

--- importchain.sh
+++ importchain.sh
@@ -1,3 +1,1 @@
-wget https://onedrive.live.com/redir?resid=391E80A214EA5AE0!3175&authkey=!AEx5yUduI2wTDho&ithint=file%2cbz2
-bzip2 -d blockchain_backup.bz2
-geth import blockchain_backup
+geth --fast
